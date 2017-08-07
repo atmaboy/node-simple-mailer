e813cd530deaf5a9c0371e2aa02b0f2101945c86
Atmaji Haryo Wiryawan <atmaji@gits.co.id>
2017-08-07 09:30:07 +0700

    update doc

--- documentation/api_project.js
+++ documentation/api_project.js
@@ -3,13 +3,13 @@ define({
   "url": "https://api.atmaji.id:3000",
   "name": "Simple Mailer",
   "version": "0.1.4",
-  "description": "Microservice to send email",
+  "description": "Microservice to send email | https://github.com/atmaboy/node-simple-mailer",
   "sampleUrl": false,
   "defaultVersion": "0.0.0",
   "apidoc": "0.3.0",
   "generator": {
     "name": "apidoc",
-    "time": "2017-08-07T02:20:06.137Z",
+    "time": "2017-08-07T02:28:11.420Z",
     "url": "http://apidocjs.com",
     "version": "0.17.6"
   }
